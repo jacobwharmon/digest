5ed64e677fc1c3dc9e58db6592c86a2571a87986
jacobwharmon <jacobwharmon@gmail.com>
2022-03-01 14:50:28 -0500

    cleaned constrution filenames and column names

--- data/construction_cleaner.py
+++ data/construction_cleaner.py
@@ -1,4 +1,12 @@
+import os
+import pandas as pd
+
+construction_path = "data/raw/construction"
+construction_files = [f'{construction_path}/{file}' for file in os.listdir(construction_path)]
+
 # Read in them all
+df_0 = pd.read_excel(construction_files[0], 1)
+df_0
 
 # See how many different formats there are
 
